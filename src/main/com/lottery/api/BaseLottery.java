58f1b7cbf0cd30382607e9f9ae7df543eae882c3
marinamitunevich <marina.mitunevich@gmail.com>
2021-10-20 23:38:58 +0200

    fixed code in BaseLottery

--- src/main/com/lottery/api/BaseLottery.java
+++ src/main/com/lottery/api/BaseLottery.java
@@ -3,6 +3,10 @@ package main.com.lottery.api;
 import main.com.lottery.exceptions.IncorrectRangeOfUnluckyNumber;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -11,6 +15,7 @@ import java.util.stream.Collectors;
 public abstract class BaseLottery implements Lottery {
 
     private static Logger log = Logger.getLogger(BaseLottery.class.getName());
+    protected Path path;
 
     protected final String lotteryName;
     protected final int minLotteryNumber;
@@ -24,33 +29,22 @@ public abstract class BaseLottery implements Lottery {
         this.maxLotteryNumber = maxLotteryNumber;
         this.lotteryCountNumbers = lotteryCountNumbers;
         this.unluckyNumbersFile = unluckyNumbersFile;
+        this.path = Paths.get(unluckyNumbersFile);
     }
 
     @Override
     public List<Integer> getUnluckyNumbers() {
 
-        List<Integer> unluckyNumbers = new LinkedList<>();
-
+        List<Integer> unluckyNumbers = new ArrayList<>();
         String line = "";
-        BufferedReader br = null;
-        StringBuffer dataFromFile = new StringBuffer();
+
         try {
-            br = new BufferedReader(new FileReader(unluckyNumbersFile));
-            while ((line = br.readLine()) != null) {
-                if (!line.isBlank())
-                    dataFromFile.append(line);
-            }
-            br.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            log.log(Level.SEVERE, "FileNotFoundException: ", e);
+            line = Files.readAllLines(path).get(0);
         } catch (IOException e) {
             e.printStackTrace();
             log.log(Level.SEVERE, "IOException: ", e);
         }
-        ;
 
-        line = dataFromFile.toString();
         unluckyNumbers = Arrays.stream(line.split(" ")).map(s -> Integer.valueOf(s)).collect(Collectors.toList());
 
         return unluckyNumbers;
@@ -64,15 +58,16 @@ public abstract class BaseLottery implements Lottery {
     @Override
     public void generateNumbers() {
 
-        List<Integer> generatedNumbers = new LinkedList<>();
+        List<Integer> generatedNumbers = new ArrayList<>();
 
         Random rand = new Random();
         while (generatedNumbers.size() < lotteryCountNumbers) {
 
             int randomNum = rand.nextInt((maxLotteryNumber - minLotteryNumber) + 1) + minLotteryNumber;
 
-            if (!generatedNumbers.contains(randomNum) && !getUnluckyNumbers().contains(randomNum))
+            if (!generatedNumbers.contains(randomNum) && !getUnluckyNumbers().contains(randomNum)) {
                 generatedNumbers.add(randomNum);
+            }
         }
 
         System.out.println(generatedNumbers.stream().sorted().collect(Collectors.toList()));
@@ -92,46 +87,50 @@ public abstract class BaseLottery implements Lottery {
             try {
                 if (!(Arrays.stream(unluckyNumbers.split(" ")).map(s -> Integer.valueOf(s))
                         .allMatch(integer -> integer <= maxLotteryNumber && integer >= minLotteryNumber)
-                        && unluckyNumbers.split(" ").length <= 6))
+                        && unluckyNumbers.split(" ").length <= 6)) {
+
                     throw new IncorrectRangeOfUnluckyNumber("unlucky number should be in the range [" + minLotteryNumber + "," +
                             maxLotteryNumber + "] and it is allowed maximum 6 unlucky numbers");
-                else
+                } else {
+
                     condition = false;
+                }
 
             } catch (IncorrectRangeOfUnluckyNumber e) {
+
                 System.out.println(e.getMessages());
                 log.log(Level.SEVERE, "IncorrectRangeOfUnluckyNumber: ", e);
 
             } catch (NumberFormatException e) {
+
                 System.out.println("invalid format of numbers");
                 log.log(Level.SEVERE, "NumberFormatException: ", e);
             }
 
         }
 
-        BufferedWriter bw = null;
+
         try {
-            bw = new BufferedWriter(new FileWriter(unluckyNumbersFile));
-            bw.write(unluckyNumbers);
-            bw.flush();
+            Files.write(path, unluckyNumbers.getBytes(StandardCharsets.UTF_8));
         } catch (IOException e) {
             e.printStackTrace();
             log.log(Level.SEVERE, "IOException: ", e);
         }
+
     }
 
     @Override
     public void removeUnluckyNumbers() {
 
-        BufferedWriter bw = null;
+        String str = "";
+
         try {
-            bw = new BufferedWriter(new FileWriter(unluckyNumbersFile));
-            bw.write("");
-            bw.flush();
+            Files.write(path, str.getBytes(StandardCharsets.UTF_8));
         } catch (IOException e) {
             e.printStackTrace();
             log.log(Level.SEVERE, "IOException: ", e);
         }
-        System.out.println("Attention unlucky numbers for "+getLotteryName()+" are deleted");
+
+        System.out.println("Attention unlucky numbers for " + getLotteryName() + " are deleted");
     }
 }
